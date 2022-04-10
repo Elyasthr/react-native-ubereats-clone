0bd2408acbd1a0637e024b7702a2b0a011224ed6
Elyas <touahria.elyas@outlook.com>
2022-04-10 19:35:07 +0200

    react-native-ubereats-clone

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from "react-native";
+import HeaderTabs from "../components/HeaderTabs";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+}
